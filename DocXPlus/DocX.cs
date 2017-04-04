bd0ff234c38e9f68d8bfaee57592e7de09739d72
GlenConway <glen.conway@kinsac.com>
2017-04-04 13:16:17 -0300

    Adding a basic test.

--- DocXPlus/DocX.cs
+++ DocXPlus/DocX.cs
@@ -1,8 +1,44 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.IO;
 
 namespace DocXPlus
 {
     public class DocX
     {
+        private WordprocessingDocument document;
+
+        public static DocX Create(string path, WordprocessingDocumentType type)
+        {
+            return DocX.Create(path, type, false);
+        }
+
+        public static DocX Create(string path, WordprocessingDocumentType type, bool autoSave)
+        {
+            var docX = new DocX();
+            docX.Create(WordprocessingDocument.Create(path, type, autoSave));
+
+            return docX;
+        }
+
+        public static DocX Create(Stream stream, WordprocessingDocumentType type, bool autoSave)
+        {
+            var docX = new DocX();
+            docX.Create(WordprocessingDocument.Create(stream, type, autoSave));
+
+            return docX;
+        }
+
+        internal void Create(WordprocessingDocument doc)
+        {
+            document = doc;
+
+            // Add a main document part. 
+            MainDocumentPart mainPart = document.AddMainDocumentPart();
+
+            // Create the document structure
+            mainPart.Document = new Document();
+        }
     }
 }
---
+++ DocXPlusTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DocXPlusTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DocXPlusTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("45dc89b0-6fe3-40a1-a75b-3ab9d2b2b596")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
