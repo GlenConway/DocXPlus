b5ee61457a2bd89aeb89d74ddce30a85d3e285b4
GlenConway <glen.conway@kinsac.com>
2017-04-14 09:37:36 -0300

    Two images test.

--- DocXPlusTests/ImageTests.cs
+++ DocXPlusTests/ImageTests.cs
@@ -37,5 +37,39 @@ namespace DocXPlusTests
 
             Launch(filename);
         }
+
+        [TestMethod]
+        public void AddTwoImages()
+        {
+            var filename = Path.Combine(TempDirectory, "AddTwoImages.docx");
+
+            var doc = DocX.Create(filename, DocumentFormat.OpenXml.WordprocessingDocumentType.Document);
+
+            var width = 0d;
+            var height = 0d;
+
+            using (System.Drawing.Image img = System.Drawing.Image.FromStream(GetEmbeddedResourceStream("Peggys_Cove_Nova_Scotia_01.jpg"), useEmbeddedColorManagement: false, validateImageData: false))
+            {
+                width = img.Width;
+                height = img.Height;
+            }
+
+            double ratio = height / width;
+
+            var inchWidth = 2;
+            var inchHeight = inchWidth * ratio;
+
+            var drawing = doc.AddImage(GetEmbeddedResourceStream("Peggys_Cove_Nova_Scotia_01.jpg"), "image/jpg", Units.InchToEMU(inchWidth), Units.InchToEMU(inchHeight));
+            doc.AddParagraph().Append(drawing);
+
+            drawing = doc.AddImage(GetEmbeddedResourceStream("Peggys_Cove_Nova_Scotia_01.jpg"), "image/jpg", Units.InchToEMU(inchWidth * 2), Units.InchToEMU(inchHeight * 2));
+            doc.AddParagraph().Append(drawing);
+
+            doc.Close();
+
+            ValidateWordDocument(filename);
+
+            Launch(filename);
+        }
     }
 }
